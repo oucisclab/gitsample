b5b5b3c0c9aac96c05cc25d6f2472d5d99160bc5
Xiaodong Wang <wangxdouc@gmail.com>
2019-05-14 23:44:11 +0800

    Test.

---
+++ src/main/java/com/uspacex/gitsample/GitsampleApplication.java
@@ -0,0 +1,17 @@
+package com.uspacex.gitsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Maintained by Xiaodong Wang
+ *
+ */
+@SpringBootApplication
+public class GitsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uspacex/gitsample/GitsampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uspacex.gitsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitsampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
